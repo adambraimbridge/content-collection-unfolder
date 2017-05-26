c14f900360a6d20dcbe1f876b3d5fdd5779f5c08
radutudor <radu.tudor@ft.com>
2017-05-26 17:58:06 +0300

    first producer version

---
+++ producer/ContentProducer.go
@@ -0,0 +1,116 @@
+package producer
+
+import (
+	"errors"
+	"github.com/Financial-Times/message-queue-go-producer/producer"
+	"github.com/Financial-Times/uuid-utils-go"
+	gouuid "github.com/satori/go.uuid"
+	log "github.com/Sirupsen/logrus"
+	"encoding/json"
+)
+
+const (
+	uriBase             = "http://content-collection-unfolder.svc.ft.com/content/"
+	cmsContentPublished = "cms-content-published"
+	methodeSystemOrigin = "http://cmdb.ft.com/systems/methode-web-pub"
+
+	//time format (also not needed atm)
+	//timeFormat = "2006-01-02T03:04:05.000Z0700"
+
+	//mapper uri bases (not needed)
+	//articleUriBase = "http://methode-article-mapper.svc.ft.com/content/"
+	//placeholderUriBase = "http://methode-content-placeholder-mapper-iw-uk-p.svc.ft.com/content/"
+	//videoUriBase = "http://next-video-mapper.svc.ft.com/video/model/"
+
+)
+
+type ContentProducer struct {
+	msgProducer producer.MessageProducer
+}
+
+func (p ContentProducer) Send(tid string, lastModified string, contentArr []map[string]interface{}) {
+	for _, content := range contentArr {
+		p.sendSingleMessage(tid, content, lastModified)
+	}
+}
+func (p ContentProducer) sendSingleMessage(tid string, content map[string]interface{}, lastModified string) {
+	logEntry := log.WithField("tid", tid)
+	uuid, err := p.extractUuid(content)
+	if nil != nil {
+		logEntry.Warnf("Skip creation of kafka message. Reason: %v ", err)
+	}
+
+	logEntry = logEntry.WithField("uuid", uuid)
+	msg, err := p.buildMessage(tid, uuid, lastModified, content)
+	if err != nil {
+		logEntry.Warnf("Skip creation of kafka message. Reason: %v ", err)
+	}
+
+	err = p.msgProducer.SendMessage("", msg)
+	if err != nil {
+		logEntry.Warnf("Unable to send message to Kafka. Reason: %v ", err)
+	}
+}
+
+func (p ContentProducer) extractUuid(content map[string]interface{}) (string, error) {
+	val, ok := content["uuid"]
+	if !ok {
+		return "", errors.New("No UUID found in content")
+	}
+
+	uuid, ok := val.(string)
+	if !ok {
+		return "", errors.New("Found UUID was not a string")
+	}
+
+	err := uuidutils.ValidateUUID(uuid)
+	if err != nil {
+		return "", err
+	}
+
+	return uuid, nil
+}
+
+func (p ContentProducer) buildMessage(tid string, uuid string, lastModified string, content map[string]interface{}) (producer.Message, error) {
+	body := publicationMessageBody{
+		ContentURI:   uriBase + uuid,
+		LastModified: lastModified,
+		Payload:      content,
+	}
+	bodyAsString, err := p.marshallToString(&body)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := map[string]string{
+		"X-Request-Id":      tid,
+		"Message-Timestamp": lastModified,
+		"Message-Id":        gouuid.NewV4().String(),
+		"Message-Type":      cmsContentPublished,
+		"Origin-System-Id":  methodeSystemOrigin,
+		"Content-Type":      "application/json",
+	}
+
+	return producer.Message{Headers: headers, Body: *bodyAsString}, nil
+
+}
+
+func (p ContentProducer) marshallToString(body *publicationMessageBody) (*string, error) {
+	binary, err := json.Marshal(*body)
+	if err != nil {
+		return nil, err
+	}
+
+	//not sure if needed
+	//binary = bytes.Replace(binary, []byte("\\u003c"), []byte("<"), -1)
+	//binary = bytes.Replace(binary, []byte("\\u003e"), []byte(">"), -1)
+
+	return &string(binary), nil
+}
+
+type publicationMessageBody struct {
+	ContentURI   string                 `json:"contentUri"`
+	LastModified string                 `json:"lastModified"`
+	Payload      map[string]interface{} `json:"payload"`
+}
+
