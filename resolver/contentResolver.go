e62f5d290b8bd54742730bb15590ac0b97024cfc
bogdanguranda <bogdan.guranda@gmail.com>
2017-05-29 15:55:49 +0300

    Removed content resolver app name as agreed.

--- resolver/contentResolver.go
+++ resolver/contentResolver.go
@@ -6,41 +6,37 @@ import (
 	"github.com/Financial-Times/transactionid-utils-go"
 	"io/ioutil"
 	"net/http"
-	"regexp"
 )
 
-var serviceNameRegex = regexp.MustCompile("__(.*?)/")
-
 type contentResolver struct {
-	contentResolverAppURI  string
-	contentResolverAppName string
-	httpClient             *http.Client
+	contentResolverAppURI string
+	httpClient            *http.Client
 }
 
 func NewContentResolver(contentResolverAppURI string) *contentResolver {
-	return &contentResolver{contentResolverAppURI: contentResolverAppURI, contentResolverAppName: serviceNameRegex.FindStringSubmatch(contentResolverAppURI)[1], httpClient: http.DefaultClient}
+	return &contentResolver{contentResolverAppURI: contentResolverAppURI, httpClient: http.DefaultClient}
 }
 
 func (cr *contentResolver) ResolveContents(uuids []string, tid string) ([]map[string]interface{}, error) {
 	resp, err := cr.callContentResolverApp(uuids, tid)
 	if err != nil {
-		return nil, fmt.Errorf("Error calling [%v] for content, error was: [%v]", cr.contentResolverAppName, err.Error())
+		return nil, fmt.Errorf("Error calling on url [%v] for content, error was: [%v]", cr.contentResolverAppURI, err.Error())
 	}
 	defer resp.Body.Close()
 
 	bodyAsBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Could not read response from [%v], transaction_id=[%v], error was: [%v]", cr.contentResolverAppName, tid, err.Error())
+		return nil, fmt.Errorf("Could not read response after calling [%v], transaction_id=[%v], error was: [%v]", cr.contentResolverAppURI, tid, err.Error())
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("Call to [%v] for transaction_id=[%v], responded with error statusCode [%d], error was: [%v]", cr.contentResolverAppName, tid, resp.StatusCode, string(bodyAsBytes))
+		return nil, fmt.Errorf("Call to [%v] for transaction_id=[%v], responded with error statusCode [%d], error was: [%v]", cr.contentResolverAppURI, tid, resp.StatusCode, string(bodyAsBytes))
 	}
 
 	var contents []map[string]interface{}
 	err = json.Unmarshal(bodyAsBytes, contents)
 	if err != nil {
-		return nil, fmt.Errorf("Could not read response body from [%v], transaction_id=[%v], error was: [%v]", cr.contentResolverAppName, tid, err.Error())
+		return nil, fmt.Errorf("Could not read response body from call to [%v], transaction_id=[%v], error was: [%v]", cr.contentResolverAppURI, tid, err.Error())
 	}
 
 	return contents, nil
@@ -49,7 +45,7 @@ func (cr *contentResolver) ResolveContents(uuids []string, tid string) ([]map[st
 func (cr *contentResolver) callContentResolverApp(uuids []string, tid string) (*http.Response, error) {
 	req, err := http.NewRequest(http.MethodGet, cr.contentResolverAppURI, nil)
 	if err != nil {
-		return nil, fmt.Errorf("Error creating request to [%v] with uri=[%v], transaction_id=[%v].", cr.contentResolverAppName, cr.contentResolverAppURI, tid)
+		return nil, fmt.Errorf("Error creating request to uri=[%v], transaction_id=[%v].", cr.contentResolverAppURI, tid)
 	}
 
 	req.Header.Set(transactionidutils.TransactionIDHeader, tid)
@@ -63,7 +59,7 @@ func (cr *contentResolver) callContentResolverApp(uuids []string, tid string) (*
 
 	resp, err := cr.httpClient.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("Error doing request to [%v] with uri=[%v], transaction_id=[%v].", cr.contentResolverAppName, cr.contentResolverAppURI, tid)
+		return nil, fmt.Errorf("Error doing request to uri=[%v], transaction_id=[%v].", cr.contentResolverAppURI, tid)
 	}
 
 	return resp, nil
