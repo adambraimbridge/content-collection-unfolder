006eae388bcf15cd6d2b79670d53f6c5307e8441
radutudor <radu.tudor@ft.com>
2017-06-09 17:37:32 +0300

    config tests (for coverage...)

---
+++ config_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/jawher/mow.cli"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDefaults(t *testing.T) {
+	app := cli.App("content-collection-unfolder", appDescription)
+
+	sc := createServiceConfiguration(app)
+
+	app.Action = func() {
+		configMap := sc.toMap()
+		assert.NotEmpty(t, configMap["appSystemCode"])
+		assert.NotEmpty(t, configMap["appName"])
+		assert.NotEmpty(t, configMap["appPort"])
+		assert.NotEmpty(t, configMap["unfoldingWhitelist"])
+		assert.NotEmpty(t, configMap["writerURI"])
+		assert.NotEmpty(t, configMap["writerHealthURI"])
+		assert.NotEmpty(t, configMap["contentResolverURI"])
+		assert.NotEmpty(t, configMap["contentResolverHealthURI"])
+		assert.NotEmpty(t, configMap["queueAddress"])
+		assert.NotEmpty(t, configMap["writeTopic"])
+		assert.NotEmpty(t, configMap["writeQueue"])
+		assert.Equal(t, "", configMap["authorization"])
+
+	}
+
+	app.Run([]string{"content-collection-unfolder"})
+}
